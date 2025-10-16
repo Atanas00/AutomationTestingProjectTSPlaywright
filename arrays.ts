9a66d1a82c2c2af5c08f18a1131ca0c8075223ad
AtanasZakov <atanaszakvo@gmail.com>
2025-10-16 19:55:55 +0300

    array methods

--- arrays.ts
+++ arrays.ts
@@ -1,8 +1,6 @@
 let numbers: number[] = [1, 2, 3, 4];
 let strings: string[] = ["a", "b", "c", "d"];
 let mixed: (number | string)[] = [1, "a", 2, "b"];
-//let pro: (any | undefined)[] = ["boolean",23,koko];
-let pro: any[] = ["boolean", 23, true];
 
 //access by index
 console.log(numbers[0]);
@@ -13,8 +11,6 @@ console.log(strings[1]);
 
 console.log(mixed[0]);
 console.log(mixed[1]);
-
-console.log(pro[0]);
 
 //array length
 console.log("array length");
@@ -43,10 +39,44 @@ console.log("hard");
 let combinate = mixed.map((element: number | string) => {
   if (typeof element === "number") {
     return element * 10;
-  }else{
-    return `Welcome ${element}`
+  } else {
+    return `Welcome ${element}`;
   }
+});
+console.log(combinate);
+//16.10
+//Filter
+console.log("16.10");
 
-})
-  console.log(combinate);
-;
+let positiveNumbers: number[] = [2, 3, 4, 5, 6];
+let filterArr: number[] = positiveNumbers.filter(
+  (num: number) => num > 3 && num < 5
+);
+console.log(filterArr);
+
+//Sort
+let sortNums: number[] = [100, 2, 30, 5, 40, 6];
+
+let sorted:number [] = sortNums.sort((a:number,b:number)=>a-b);
+console.log(sorted);
+
+//Push >>> add element to arr on last place
+sorted.push(200);
+console.log(sorted);
+
+//pop >>> remove last element from arr 
+
+sorted.pop();
+console.log(sorted);
+
+//shift remove first element from arr
+
+sorted.shift();
+console.log(sorted);
+
+//unshift add new element on first place of arr 
+// before [5, 6, 30, 40, 100]
+sorted.unshift(1);
+console.log(sorted);
+//after [1, 5, 6, 30, 40, 100] 
+
